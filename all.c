a7c47e5544a60d10d57d476d2ee51ef6da58a122
Yutaro Yoshii <yakumodanshi@gmail.com>
2021-06-16 17:32:41 +0900

    added the brainfuck character condition to the header

---
+++ all.c
@@ -0,0 +1,9 @@
+# include<stdio.h>
+
+int main(int argc, char * argv){
+    //parsing the arguments
+    int i;
+    for(i = 0; i < argv; i++){
+
+    }
+}
--- all.h
+++ all.h
@@ -18,3 +18,15 @@ enum INSTR
 }
 
 #endif
+
+#define SUCCESS 0
+#define FAILURE 1
+#define BFCHARS(EXP) EXP == 43 || \
+EXP == 44 || \
+EXP == 45 || \
+EXP == 46 || \
+EXP == 60 || \
+EXP == 62 || \
+EXP == 91 || \
+EXP == 93
+

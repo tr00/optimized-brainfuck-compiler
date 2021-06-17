9e8dce5bff41ba40c28d0b172e8100ed14e5f4f7
tr00 <tryyyy@gmx.de>
2021-06-17 12:54:18 +0200

    added TODO list

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # optimized-brainfuck-compiler
 test
+
+## TODO
+
+    * unified error handling 
+    * preprocessor
+    * virtual machine backend
+    * more profound optimizers (maybe?)
+    * compiling backend
---
+++ src/virtualmachine.c
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+#include "all.h"
+
+void run(uint8_t *prog)
+{
+
+    uint8_t opcode;
+    uint8_t *tape;
+    size_t pc;
+
+    while (1)
+    {
+        opcode = prog[pc];
+
+        switch (opcode)
+        {
+        }
+
+        pc++;
+    }
+}
